977752431f23334b0c6058286fd12275ced66a93
lazyaccountant <120103749+lazyaccountant@users.noreply.github.com>
2023-07-21 00:28:34 +0100

    Update Overview.py
    UI changes to compare page

--- Streamlit/Overview.py
+++ Streamlit/Overview.py
@@ -11,7 +11,7 @@ st.set_page_config(
     page_icon="📈",
 )
 
-with open("Streamlit/company_data.json") as json_file:
+with open("company_data.json") as json_file:
     data = json.load(json_file)
 
 daysCount = daysCount()
@@ -19,6 +19,7 @@ daysCount = daysCount()
 
 period_dict = {"1M": 21, "3M": 63, "6M": 126, "1Y": 252, "YTD": daysCount, "MAX": 0}
 
+no_of_comp = 0
 
 option = st.selectbox("Enter Company Name", list(data.keys()))
 period = st.sidebar.selectbox("Period", list(period_dict.keys()))
@@ -27,38 +28,61 @@ comp = st.sidebar.selectbox("Compare with...", [""]+list(data.keys()))
 dataframe = get_data(data[option], "2010-01-01", str(date.today()))
 returns = dataframe.pct_change().dropna(how="all")
 
+def delta(df, period):
+    lastPrice = df.iloc[-1]
+    previousPrice = df.iloc[-1*period]
+    period_ret = (lastPrice / previousPrice) - 1
+    return str(round(period_ret*100, 2)) + "%"
+
 def defaultchart(company_name, df, period, duration):
     lastPrice = df["Price"].iloc[-1]
     previousDayPrice = df["Price"].iloc[-2]
     previousPrice = df["Price"].iloc[-1*period]
     period_ret = (lastPrice / previousPrice) - 1
+
     st.write(f"""
         # {company_name}""")
+    
     col1, col2, col3 = st.columns(3)
-    col1.write(f"""# ₦{df["Price"].iloc[-1]}
-        """)
-    col2.metric("1D Return",
-                "₦" + str(round(lastPrice-previousDayPrice, 2)),
-                delta=str(round(returns["Price"].iloc[-1] * 100, 2)) + "%")
-    col3.metric(f"{duration} Return",
-                "₦" + str(round(lastPrice-previousPrice, 2)),
-                delta=str(round(period_ret*100, 2)) + "%")
+
+    col1.metric(
+        "Price",
+        f"₦{df['Price'].iloc[-1]}"
+        )
+    
+    col2.metric(
+        "1D Return",
+        "₦" + str(round(lastPrice-previousDayPrice, 2)),
+        delta=str(round(returns["Price"].iloc[-1] * 100, 2)) + "%"
+    )
+    col3.metric(
+        f"{duration} Return",
+        "₦" + str(round(lastPrice-previousPrice, 2)),
+        delta=str(round(period_ret*100, 2)) + "%"
+    )
+
     fig = px.line(df.iloc[-1*period:], labels={"value": "Price (₦)"})
+
     fig.update_layout(showlegend=False)
-    st.plotly_chart(fig, use_container_width=True)
+    st.plotly_chart(fig, use_container_width=False)
 
 
-def comparechart(company_name, comp_name, df, period): 
+def comparechart(company_name, comp_name, df, period):
+    comp1 = df[df.columns[0]]
+    comp2 = df[df.columns[1]]
+
     col1, col2 = st.columns(2)
-    col1.write(f"""
-        # {company_name}""")
-    col1.write(f"""# ₦{df[df.columns[0]].iloc[-1]}
-        """)
+    col1.metric(
+        label=f"{company_name}",
+        value=f"₦{comp1.iloc[-1]}",
+        delta=delta(comp1, period)
+    )
 
-    col2.write(f"""
-        # {comp_name}""")
-    col2.write(f"""# ₦{df[df.columns[1]].iloc[-1]}
-        """)
+    col2.metric(
+        label=f"{comp_name}",
+        value=f"₦{comp2.iloc[-1]}",
+        delta=delta(comp2, period)
+    )
 
     pf = df.iloc[-1*period:]
     pf = (pf/pf.iloc[0] - 1) * 100
@@ -66,7 +90,7 @@ def comparechart(company_name, comp_name, df, period):
     fig = px.line(pf, labels={"value": "Returns (%)"})
     fig.update_layout(legend=dict(title=None, yanchor="top", y=0.99, xanchor="left", x=0.01))
 
-    st.plotly_chart(fig, use_container_width=True)
+    st.plotly_chart(fig)
     #st.line_chart(pf, x="Date", y="Returns (%)")
 
 
